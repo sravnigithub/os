13a1313fbebdc294c520abf49797fcfa958ba790
sravnigithub <124333710+sravnigithub@users.noreply.github.com>
2023-05-14 22:29:09 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,117 +1,70 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
-
 {
-
-    int pid[15];
-
-    int bt[15];
-
-    int n;
-
-    printf("Enter the number of processes: ");
-
+    int  p[10],at[10],bt[10],ct[10],tat[10],wt[10],i,j,temp=0,n;
+    float awt=0,atat=0;
+    printf("enter no of proccess you want:");
     scanf("%d",&n);
-
- 
-
-    printf("Enter process id of all the processes: ");
-
-    for(int i=0;i<n;i++)
-
+    printf("enter %d process:",n);
+    for(i=0;i<n;i++)
     {
-
-        scanf("%d",&pid[i]);
-
+    scanf("%d",&p[i]);
     }
-
- 
-
-    printf("Enter burst time of all the processes: ");
-
-    for(int i=0;i<n;i++)
-
+    printf("enter %d arrival time:",n);
+    for(i=0;i<n;i++)
     {
-
-        scanf("%d",&bt[i]);
-
+    scanf("%d",&at[i]);
     }
-
- 
-
-    int i, wt[n];
-
-    wt[0]=0;
-
- 
-
-    //for calculating waiting time of each process
-
-    for(i=1; i<n; i++)
-
+    printf("enter %d burst time:",n);
+    for(i=0;i<n;i++)
     {
-
-        wt[i]= bt[i-1]+ wt[i-1];
-
+    scanf("%d",&bt[i]);
     }
-
- 
-
-    printf("Process ID     Burst Time     Waiting Time     TurnAround Time\n");
-
-    float twt=0.0;
-
-    float tat= 0.0;
-
-    for(i=0; i<n; i++)
-
+    // sorting at,bt, and process according to at
+    for(i=0;i<n;i++)
     {
-
-        printf("%d\t\t", pid[i]);
-
-        printf("%d\t\t", bt[i]);
-
-        printf("%d\t\t", wt[i]);
-
- 
-
-        //calculating and printing turnaround time of each process
-
-        printf("%d\t\t", bt[i]+wt[i]);
-
-        printf("\n");
-
- 
-
-        //for calculating total waiting time
-
-        twt += wt[i];
-
- 
-
-        //for calculating total turnaround time
-
-        tat += (wt[i]+bt[i]);
-
+     for(j=0;j<(n-i);j++)
+    {
+      if(at[j]>at[j+1])
+     {
+        temp=p[j+1];
+        p[j+1]=p[j];
+        p[j]=temp;
+        temp=at[j+1];
+        at[j+1]=at[j];
+        at[j]=temp;
+        temp=bt[j+1];
+        bt[j+1]=bt[j];
+        bt[j]=temp;
+      }
+     }
     }
-
-    float att,awt;
-
- 
-
-    //for calculating average waiting time
-
-    awt = twt/n;
-
- 
-
-    //for calculating average turnaround time
-
-    att = tat/n;
-
-    printf("Avg. waiting time= %f\n",awt);
-
-    printf("Avg. turnaround time= %f",att);
-
+    /* calculating 1st ct */
+    ct[0]=at[0]+bt[0];
+    /* calculating 2 to n ct */
+    for(i=1;i<n;i++)
+    {
+temp=0;
+     if(ct[i-1]<at[i])
+{
+temp=at[i]-ct[i-1];
+}
+ct[i]=ct[i-1]+bt[i]+temp;
+}   printf("\np\t A.T\t B.T\t C.T\t TAT\t WT");
+    for(i=0;i<n;i++)
+    {
+    tat[i]=ct[i]-at[i];
+    wt[i]=tat[i]-bt[i];
+    atat+=tat[i];
+    awt+=wt[i];
+    }
+    atat=atat/n;
+    awt=awt/n;
+    for(i=0;i<n;i++)
+    {
+      printf("\nP%d\t %d\t %d\t %d \t %d \t %d",p[i],at[i],bt[i],ct[i],tat[i],wt[i]);
+    }
+    printf("\naverage turnaround time is %f",atat);
+  printf("\naverage wating timme is %f",awt);
+    return 0;
 }
