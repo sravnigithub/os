ea9b1f511aec1fd7adf61817733bc0c7752ec264
sravnigithub <124333710+sravnigithub@users.noreply.github.com>
2023-05-14 22:37:51 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,51 +1,75 @@
-#include <stdio.h>
+#include<stdio.h>
 void swap(int *a,int *b)
 {
-int temp=*a;
+int temp =*a;
 *a=*b;
 *b=temp;
 }
 int main()
 {
-int n;
-printf("Enter Number of Processes: ");
+int n,i;
+float sum=0,sum1=0;
+float awt,atat;
+printf("enter number of processes:");
 scanf("%d",&n);
-int b[n],p[n],index[n];
+int p[n],bt[n],ct[n],wt[n],at[n],tat[n];
+printf("enter arrival times:");
 for(int i=0;i<n;i++)
 {
-printf("Enter Burst Time and Priority Value for Process %d: ",i+1);
-scanf("%d %d",&b[i],&p[i]);
-index[i]=i+1;
+scanf("%d",&at[i]);
+}
+printf("enter burst times:");
+for(int i=0;i<n;i++)
+{
+scanf("%d",&bt[i]);
 }
 for(int i=0;i<n;i++)
 {
-int a=p[i],m=i;
-for(int j=i;j<n;j++)
+for(int j=0;j<n-1;j++)
 {
-if(p[j] > a)
+if (bt[j]>bt[j+1])
 {
-a=p[j];
-m=j;
-}
-}
-swap(&p[i], &p[m]);
-swap(&b[i], &b[m]);
-swap(&index[i],&index[m]);
-}
-int t=0;
-printf("Order of process Execution is\n");
+
+swap(&bt[j],&bt[j+1]);
+swap(&at[j],&at[j+1]);
+}}}
+
 for(int i=0;i<n;i++)
 {
-printf("P%d is executed from %d to %d\n",index[i],t,t+b[i]);
-t+=b[i];
+if(at[i]==0)
+{
+ct[i]=bt[i];
 }
-printf("\n");
-printf("Process Id     Burst Time   Wait Time    TurnAround Time\n");
-int wait_time=0;
+else if(at[i]>ct[i-1])
+{
+ct[i]=ct[i-1]+bt[i]+(at[i]-ct[i-1]);
+}
+else
+{
+ct[i]=ct[i-1]+bt[i];
+}
+}
 for(int i=0;i<n;i++)
 {
-printf("P%d          %d          %d          %d\n",index[i],b[i],wait_time,wait_time + b[i]);
-wait_time += b[i];
+tat[i]=ct[i]-at[i];
 }
-return 0;
+for(int i=0;i<n;i++)
+{
+wt[i]=tat[i]-bt[i];
+}
+for(int i=0;i<n;i++)
+{
+sum+=wt[i];
+sum1+=tat[i];
+}
+awt=sum/n;
+atat=sum1/n;
+printf("AT\t\tBT\t\tCT\t\tTAT\t\tWT\n");
+for(int i=0;i<n;i++)
+{
+printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",at[i],bt[i],ct[i],tat[i],wt[i]);
+}
+
+printf("avg wait time=%f",awt);
+printf("avg turnaround time=%f",atat);
 }
